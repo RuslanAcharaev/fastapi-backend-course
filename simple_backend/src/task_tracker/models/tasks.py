6c05c35249a9223ee26c2397fa57da83fa39e330
Ruslan Acharaev <racharaev@gmail.com>
2025-08-04 14:35:37 +0300

    update models\tasks.py

--- simple_backend/src/task_tracker/models/tasks.py
+++ simple_backend/src/task_tracker/models/tasks.py
@@ -6,8 +6,8 @@ from pydantic import BaseModel
 class TaskIn(BaseModel):
     title: str
     description: Optional[str] = None
+    status: Optional[bool] = False
 
 
 class TaskDB(TaskIn):
     id: int
-    status: bool = False
